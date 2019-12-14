0a89fd37f95c5fea010b4eafab7ca51797892b96
JuanDevWP <blackmaxxgdl18@hotmail.com>
2019-12-13 18:48:14 -0600

    agregando markup para componentes

--- src/components/EditarProducto.js
+++ src/components/EditarProducto.js
@@ -2,7 +2,44 @@ import React from 'react';
 
 const EditarProducto = () => {
     return ( 
-        <h1>Desde EditarProducto.js</h1>
+        <div className="row justify-content-center">
+            <div className="col-md-8">
+                <div className="card">
+                    <div className="card-body">
+                        <h2 className="text-center mb-4 font-weight-bold">
+                            Editar Producto
+                        </h2>
+
+                        <form>
+                            <div className="form-group">
+                                <label>Nombre Producto</label>
+                                <input
+                                    type="text"
+                                    className="form-control"
+                                    placeholder="Nombre Producto"
+                                    name="nombre"
+                                />
+                            </div>
+
+                            <div className="form-group">
+                                <label>Precio Producto</label>
+                                <input
+                                    type="number"
+                                    className="form-control"
+                                    placeholder="Precio Producto"
+                                    name="precio"
+                                />
+                            </div>
+
+                            <button 
+                                type="submit"
+                                className="btn btn-primary font-weight-bold text-uppercase d-block w-100"
+                            >Guardar Cambios</button>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
      );
 }
  
--- src/components/Productos.js
+++ src/components/Productos.js
@@ -1,8 +1,23 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 
 const Productos = () => {
     return ( 
-        <h1>Desde Productos.js</h1>
+       <Fragment>
+           <h2 className="text-center my-5">Listado de Productos</h2>
+
+           <table className="table table-striped">
+               <thead className="bg-primary table-dark">
+                    <tr>
+                        <th scope="col">Nombre</th>
+                        <th scope="col">Precio</th>
+                        <th scope="col">Acciones</th>
+                    </tr>
+               </thead>
+               <tbody>
+                   
+               </tbody>
+           </table>
+       </Fragment>
      );
 }
  
