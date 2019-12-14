bf0a255109d5b6ddadc2c7365b64ed5532c83854
JuanDevWP <blackmaxxgdl18@hotmail.com>
2019-12-14 14:03:03 -0600

    agregando las primeras funciones de action y reducer

--- src/actions/productoActions.js
+++ src/actions/productoActions.js
@@ -6,7 +6,29 @@ import {
 
 // Crear nuevos productos
 export function crearNuevoProductoAction(producto) {
-    return () => {
-        console.log(producto)
+    return (dispatch) => {
+        dispatch( agregarProducto() );
+
+        try {
+            dispatch( agregarProductoExito(producto) );
+        } catch (error) {
+            dispatch( agregarProductoError(true) );
+        }
     }
 }
+
+const agregarProducto = () => ({
+    type: AGREGAR_PRODUCTO,
+    payload: true
+});
+
+// si el producto se guarda en la base de datos
+const agregarProductoExito = producto => ({
+    type: AGREGAR_PRODUCTO_EXITO,
+    payload: producto
+})
+
+// si hubo un error
+const agregarProductoError = estado => ({
+
+})
--- src/reducers/productosReducer.js
+++ src/reducers/productosReducer.js
@@ -13,6 +13,19 @@ const initialState = {
 
 export default function(state = initialState, action) {
     switch(action.type) {
+        case AGREGAR_PRODUCTO: 
+            return {
+                ...state,
+                loading: action.payload
+            }
+        case AGREGAR_PRODUCTO_EXITO:
+            return {
+                ...state,
+                loading: false,
+                productos: [...state.productos, action.payload]
+            }
+
+
         default:
             return state;
     }
