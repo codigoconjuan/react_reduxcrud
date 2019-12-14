013910e93dfa64cde42035a76bb093a1079476df
JuanDevWP <blackmaxxgdl18@hotmail.com>
2019-12-14 12:56:30 -0600

    agregando types

---
+++ src/actions/productoActions.js
@@ -0,0 +1,5 @@
+import {
+    AGREGAR_PRODUCTO,
+    AGREGAR_PRODUCTO_EXITO,
+    AGREGAR_PRODUCTO_ERROR
+} from '../types';
--- src/reducers/productosReducer.js
+++ src/reducers/productosReducer.js
@@ -1,3 +1,9 @@
+import {
+    AGREGAR_PRODUCTO,
+    AGREGAR_PRODUCTO_EXITO,
+    AGREGAR_PRODUCTO_ERROR
+} from '../types';
+
 // cada reducer tiene su propio state
 const initialState = {
     productos: [],
