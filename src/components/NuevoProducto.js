c517bf526c062adda4c56608a9b58f3a844f71b2
JuanDevWP <blackmaxxgdl18@hotmail.com>
2019-12-14 15:06:02 -0600

    leyendo los datos del state

--- src/components/NuevoProducto.js
+++ src/components/NuevoProducto.js
@@ -13,6 +13,11 @@ const NuevoProductos = () => {
     // utilizar use dispatch y te crea una función
     const dispatch = useDispatch();
 
+    // Acceder al state del store
+    const cargando = useSelector( state => state.productos.loading );
+    const error = useSelector(state => state.productos.error);
+
+
 
     // mandar llamar el action de productoAction
     const agregarProducto = producto => dispatch( crearNuevoProductoAction(producto) );
@@ -77,6 +82,10 @@ const NuevoProductos = () => {
                                 className="btn btn-primary font-weight-bold text-uppercase d-block w-100"
                             >Agregar</button>
                         </form>
+
+                        { cargando ? <p>Cargando...</p> : null }
+                        
+                        { error ? <p className="alert alert-danger p2 mt-4 text-center">Hubo un error</p> : null }
                     </div>
                 </div>
             </div>
