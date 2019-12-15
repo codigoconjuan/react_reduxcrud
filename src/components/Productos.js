6100347c17e195457f0be4be5faadddf3010c6e1
JuanDevWP <blackmaxxgdl18@hotmail.com>
2019-12-14 18:08:31 -0600

    mostrando el state de error y loading

--- src/components/Productos.js
+++ src/components/Productos.js
@@ -19,11 +19,16 @@ const Productos = () => {
 
     // obtener el state
     const productos = useSelector( state => state.productos.productos );
-    console.log(productos)
+    const error = useSelector(state => state.productos.error);
+    const cargando = useSelector(state => state.productos.loading);
 
     return ( 
        <Fragment>
            <h2 className="text-center my-5">Listado de Productos</h2>
+
+           { error ? <p className="font-weight-bold alert alert-danger text-center mt-4">Hubo un error</p> : null }
+           
+           { cargando ? <p className="text-center">Cargando....</p> : null }
 
            <table className="table table-striped">
                <thead className="bg-primary table-dark">
