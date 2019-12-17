077145d234b5edc72b617fd4406388e71d7c9414
JuanDevWP <blackmaxxgdl18@hotmail.com>
2019-12-16 18:52:59 -0600

    redireccionando de forma programada

--- src/components/Producto.js
+++ src/components/Producto.js
@@ -1,6 +1,7 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useHistory } from 'react-router-dom';
 import Swal from 'sweetalert2';
+
 
 // Redux
 import { useDispatch } from 'react-redux';
@@ -10,6 +11,7 @@ const Producto = ({producto}) => {
     const { nombre, precio, id } = producto;
 
     const dispatch = useDispatch();
+    const history = useHistory(); // habilitar history para redirección
 
     // Confirmar si desea eliminarlo
     const confirmarEliminarProducto = id => {
@@ -32,15 +34,22 @@ const Producto = ({producto}) => {
         });
     }
 
+    // función que redirige de forma programada
+    const redireccionarEdicion = producto => {
+        history.push(`/productos/editar/${producto.id}`)
+    }
+
     return ( 
         <tr>
             <td>{nombre}</td>
             <td><span className="font-weight-bold"> $ {precio} </span></td>
             <td className="acciones">
-                <Link 
-                    to={`/productos/editar/${id}`} className="btn btn-primary mr-2">
+                <button 
+                    type="button"
+                    onClick={ () => redireccionarEdicion(producto) }
+                    className="btn btn-primary mr-2">
                     Editar
-                </Link>
+                </button>
                 <button 
                     type="button"
                     className="btn btn-danger"
